cbc8332af59df1ba71d11e39e62389e46e86f924
Kevin Bonham <kevbonham@gmail.com>
2020-04-01 20:05:04 -0400

    start adding API tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,26 @@ using GPTool
 using GPTool.DataFrames
 using GPTool.CSV
 using Test
+using Random
 
 @testset "API" begin
-    
+    Random.seed!(42)
+
+    gp = GPTool.parse_gp_formula("y : Gaussian(.01) ~| SExp(t; l=1.5)*Constant(1)", ["t", "y"], [false, false])
+    @test gp isa GPTool.ParsedGPFormula
+    @test length(gp.xnames) == 1
+    @test gp.xnames[1] == "t"
+    # what else ??
+
+    t = collect(-5.:0.1:5.)
+    f, y = GPTool.samplegp(gp.gp, [], [], [], [], t, t)
+    @test length(f) == length(y)
+    @test first(f) == 0.1393494648232693
+    @test first(y) == 0.14029547194060588
+
+    gp = GPTool.parse_gp_formula("y*Reads/100 : Binomial(Reads) ~| Cat(Person) * SExp(Time) + Noise", ["Time", "y", "Reads", "Person"], [false, false, false, true])
+    @test gp.xfun_params == [:Person, :Time]
+    @test gp.θc == [0.1,1.,1.]
 end
 
 
