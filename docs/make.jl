56c8edc6a14dff585ecb601c88442df92ef31d3d
Kevin Bonham <kevbonham@gmail.com>
2020-06-24 14:11:56 -0400

    fix docs make

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,16 @@ using Documenter
 
 makedocs(;
     modules=[GPTool],
-    authors=["Kevin Bonham, PhD", "Jason Lloyde-Price, PhD"]
+    authors=["Kevin Bonham, PhD", "Jason Lloyde-Price, PhD"],
     repo="https://github.com/biobakery/gptool.jl/blob/{commit}{path}#L{line}",
     sitename="gptool.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://biobakery.github.io/gptool.jl",
-        assets=String[],
+        assets=String[]
     ),
     pages=[
-        "Home" => "index.md",
+        "Home" => "index.md"
     ],
 )
 
