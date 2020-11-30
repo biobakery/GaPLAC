6fb832c8db2dfa25acb750b40d0bce240fd316e6
Kevin Bonham <kevbonham@gmail.com>
2020-11-30 17:13:31 -0500

    final model with plots

--- scratch.jl
+++ scratch.jl
@@ -12,47 +12,38 @@ using Random
 # using ReverseDiff
 # using Zygote
 # Turing.setadbackend(:forwarddiff)
-ip1 = CSV.File("test/testin/input_pair_3206.tsv") |> DataFrame
+ip1 = CSV.File("test/testin/input_pair_1609.tsv") |> DataFrame
 ip1 = ip1[completecases(ip1), :]
 pidmap = Dict(p=>i for (i,p) in enumerate(unique(ip1.PersonID)))
 ip1.pid = [pidmap[p] for p in ip1.PersonID]
-ip1.bugmod = [b+rand(Normal(0, 0.1)) for b in ip1.bug]
-ip1.datemod = [d+rand(Normal(0, 0.5)) for d in ip1.Date]
+ip1.datemod = [d+rand(Normal(0, 0.1)) for d in ip1.Date]
 
-@chain ip1 begin
-    groupby(:pid)
-    transform!(:pid => length => :nsamples)
-end
-ip1f = filter(:nsamples => ==(4), ip1)
+# sekernel(alpha, rho) = 
+#   alpha^2 * KernelFunctions.transform(SEKernel(), sqrt(0.5)/rho)
 
-sekernel(alpha, rho) = 
-  alpha^2 * KernelFunctions.transform(SEKernel(), sqrt(0.5)/rho)
-
-function subjectcorrmat(subjects, t=T) where T
+function subjectcorrmat(subjects)
     n = length(subjects)
-    scov = zeros(t, n, n)
-    # if same person, 1.0, otherwise, 0.0
+    scov = zeros(Bool, n, n)
+    # if same person, true, otherwise, false
     for i in 1:n, j in 1:n
-        subjects[i] == subjects[j] && (scov[i,j] = one(t))
+        subjects[i] == subjects[j] && (scov[i,j] = true)
     end
     return scov
 end
 
-@model function GPmodel1(bug, diet, subj, tp, jitter=1e-6, T=Float64)
+@model function GPmodel1(bug, diet, subjcorr, tp, jitter=1e-6, T=Float64)
     nobs = length(bug)
     # assume zero mean
     μ = zeros(T, nobs)
     
     # diet covariance
-    c ~ LogNormal()
+    c ~ Normal(0, 1)
     K_diet = kernelmatrix(LinearKernel(c=c), reshape(diet, length(diet),1), obsdim=1)
     
     # time covariance
     K_time = kernelmatrix(Matern52Kernel(), reshape(tp, length(tp), 1), obsdim=1)  # cov matrix for time
-    
     # remove cross-person covariance
-    scov = subjectcorrmat(subj, T)
-    K_time = K_time .* scov
+    K_time = K_time .* subjcorr
     
     # jitter for numerical stability
     σ2 ~ LogNormal(0, 1)
@@ -61,7 +52,8 @@ end
     bug ~ MvNormal(μ, K_time .+ K_diet)
 end
 
-gpm1 = GPmodel1(ip1.bug, ip1.nutrient, ip1.pid, ip1.datemod)
+scorr = subjectcorrmat(ip1.pid)
+gpm1 = GPmodel1(ip1.bug, ip1.nutrient, scorr, ip1.datemod)
 @time r1 = sample(gpm1, HMC(0.1,20), 100)
 
 @model function GPmodel2(bug, subj, tp, jitter=1e-6, T=Float64)
@@ -80,11 +72,14 @@ gpm1 = GPmodel1(ip1.bug, ip1.nutrient, ip1.pid, ip1.datemod)
     σ2 ~ LogNormal(0, 1)
     K_time += LinearAlgebra.I * (σ2 + jitter)
 
-    bug ~ MvNormal(μ, K_time .+ K_diet)
+    bug ~ MvNormal(μ, K_time)
 end
 
-gpm2 = GPmodel2(ip1.bug[1:50], ip1.pid[1:50], ip1.Date[1:50])
+gpm2 = GPmodel2(ip1.bug, ip1.pid, ip1.datemod)
 @time r2 = sample(gpm2, HMC(0.1,20), 100);
+
+# log bayes
+log2(mean(r1[:lp] .^2) / mean(r2[:lp] .^2))
 
 
 @model function mixedmodel1(bug, diet, subj)
@@ -102,6 +97,9 @@ end
 mm1 = mixedmodel1(ip1.nutrient, ip1.bug, ip1.pid)
 @time rmm1 = sample(mm1, HMC(0.1,20), 100)
 
+# log bayes GP vs mixed model
+log2(mean(r1[:lp] .^2) / mean(rmm1[:lp] .^2))
+
 @model function mixedmodel2(bug, subj)
     num_subjects = length(unique(subj))  # number of subjects
     
@@ -117,9 +115,8 @@ mm2 = mixedmodel2(ip1.bug, ip1.pid)
 @time rmm2 = sample(mm2, HMC(0.1,20), 100)
 
 
-
 # log bayes
-log2(mean(rmm1[:lp]) / mean(rmm2[:lp]))
+log2(mean(rmm1[:lp] .^2) / mean(rmm2[:lp] .^2))
 
 rmm1[:diet_effect]
 
@@ -130,21 +127,44 @@ plot(rmm1)
 ############
 
 using StatsPlots
-using Pipe
-@pipe ip1 |> groupby(_, :pid) |> transform!(_, :pid => length => :nsamples)
-filter!(:nsamples => ==(4), ip1)
+using Chain
 
-@pipe ip1 |> groupby(_, :pid) |> 
-        transform!(_, AsTable(:) => 
-            (t-> (t.Date .- minimum(t.Date)) .+ 1) => :normDate)
-ip1.normDate
+@chain ip1 begin
+    groupby(:pid)
+    transform!(:pid => length => :nsamples)
+end
 
+@chain ip1 begin
+    groupby(:pid)
+    transform!(
+        AsTable(:) => 
+        (t-> (t.Date .- minimum(t.Date)) .+ 1) => 
+        :normDate)
+end
 
-@df ip1 plot(:normDate, :bug, group=:pid, legend=false,
-             xlabel="Time", ylabel="Bug")
+@chain ip1 begin
+    groupby(:pid)
+    transform!(
+        AsTable(:) => 
+        (t-> (t.datemod .- minimum(t.datemod)) .+ 1) => 
+        :normDatemod)
+end
+
+@df ip1 scatter(:normDate, :bug, color=:gray,
+             xlabel="Time", ylabel="Bug", label="Date")
+@df ip1 scatter!(:normDatemod, :bug, color=:blue, alpha=0.2,
+            xlabel="Time", ylabel="Bug", label="Date + rand")
+
+@df filter(:normDate => <(20), ip1) scatter(:normDate, :bug, color=:gray,
+                                            xlabel="Time", ylabel="Bug", label="Date")
+@df filter(:normDate => <(20), ip1) scatter!(:normDatemod, :bug, color=:blue, alpha=0.2,
+                                            xlabel="Time", ylabel="Bug", label="Date + rand")
+
 
 @df ip1 plot(:normDate, :nutrient, group=:pid, legend=false,
              xlabel="Time", ylabel="Diet")
 
 @df ip1 scatter(:nutrient, :bug, legend=false,
                 ylabel="Bug", xlabel="Diet")
+
+plot(r1)
